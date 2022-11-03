ec72340c6bccf54bf413e93951a766cdf37da209
GustavoBatistaDev <102866009+GustavoBatistaDev@users.noreply.github.com>
2022-11-03 17:06:40 -0300

    Update settings.py

--- valemoveis/settings.py
+++ valemoveis/settings.py
@@ -139,7 +139,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST_USER = 'dkssnowsnow@gmail.com'
-EMAIL_HOST_PASSWORD = "wdpjqtlwlkrkkhye"
+EMAIL_HOST_PASSWORD= ''
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
